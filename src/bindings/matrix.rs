85936146c5592054fd8461c28107109346254746
Simon Ask Ulsnes <simon@ulsnes.dk>
2022-04-19 07:54:24 +0200

    Remove redundant docs

--- src/bindings/matrix.rs
+++ src/bindings/matrix.rs
@@ -3,6 +3,7 @@ use super::*;
 /// Trait describing a glam N x N matrix type.
 ///
 /// Note: All glam matrices are square.
+#[allow(missing_docs)]
 pub trait Matrix:
     ValueSemantics
     + Add<Self, Output = Self>
@@ -16,39 +17,21 @@ pub trait Matrix:
     + Neg<Output = Self>
     + AbsDiffEq<Epsilon = <Self::Scalar as AbsDiffEq>::Epsilon>
 {
-    /// The component type of the `glam` matrix. Either `f32` or `f64`.
     type Scalar: PrimitiveMatrices<Vec2 = Self::Vec2, Vec3 = Self::Vec3, Vec4 = Self::Vec4>
         + Float
         + AbsDiffEq;
-    /// Shorthand for `glam::Vec2` or `glam::DVec2`.
     type Vec2: Vector<2, Scalar = Self::Scalar>;
-    /// Shorthand for `glam::Vec3` or `glam::DVec3`.
     type Vec3: Vector<3, Scalar = Self::Scalar>;
-    /// Shorthand for `glam::Vec4` or `glam::DVec4`.
     type Vec4: Vector<4, Scalar = Self::Scalar>;
 
-    /// True if any element is NaN.
     #[must_use]
     fn is_nan(&self) -> bool;
-
-    /// True if all elements are finite and non NaN.
     #[must_use]
     fn is_finite(&self) -> bool;
-
-    /// Transpose the matrix.
     #[must_use]
     fn transpose(&self) -> Self;
-
-    /// Invert the matrix.
-    ///
-    /// Note: If the matrix is not invertible, this returns an invalid matrix.
-    /// See (e.g.) [`glam::Mat4::inverse()`].
     #[must_use]
     fn inverse(&self) -> Self;
-
-    /// Matrix determinant.
-    ///
-    /// If the determinant is non-zero, the matrix is invertible.
     #[must_use]
     fn determinant(&self) -> Self::Scalar;
 }
@@ -56,62 +39,29 @@ pub trait Matrix:
 /// Primitive 2x2 matrix.
 ///
 /// Implemented for [`glam::Mat2`] and [`glam::DMat2`].
-pub trait Matrix2: Matrix + Mul<Self::Vec2, Output = Self::Vec2> {
-    /// Transform vector or point.
-    ///
-    /// See [`glam::Mat2::mul_vec2()`] or
-    /// [`glam::DMat2::mul_vec2()`].
+#[allow(missing_docs)]
+pub trait Matrix2: Matrix {
     fn mul_vec2(&self, vec: Self::Vec2) -> Self::Vec2;
-
-    /// Create from column vectors.
     fn from_cols(x_axis: Self::Vec2, y_axis: Self::Vec2) -> Self;
-
-    /// Get column at `index`.
     fn col(&self, index: usize) -> Self::Vec2;
-    /// Get row at `index`.
     fn row(&self, index: usize) -> Self::Vec2;
-
-    /// 2D scaling matrix.
     fn from_scale_angle(vector: Self::Vec2, angle: Self::Scalar) -> Self;
-
-    /// 2D rotation matrix.
     fn from_angle(angle: Self::Scalar) -> Self;
 }
 
 /// Primitive 3x3 matrix.
 ///
 /// Implemented for [`glam::Mat3`] and [`glam::DMat3`].
+#[allow(missing_docs)]
 pub trait Matrix3: Matrix + Mul<Self::Vec3, Output = Self::Vec3> {
-    /// Create from column vectors.
     fn from_cols(x_axis: Self::Vec3, y_axis: Self::Vec3, z_axis: Self::Vec3) -> Self;
-
-    /// Get column at `index`.
     fn col(&self, index: usize) -> Self::Vec3;
-    /// Get row at `index`.
     fn row(&self, index: usize) -> Self::Vec3;
-
-    /// Transform vector.
-    ///
-    /// See [`glam::Mat3::transform_vector2()`] or
-    /// [`glam::DMat3::transform_vector2()`].
     fn transform_vector2(&self, vector: Self::Vec2) -> Self::Vec2;
-
-    /// Transform point.
-    ///
-    /// See [`glam::Mat3::transform_point2()`] or
-    /// [`glam::DMat3::transform_point2()`].
     fn transform_point2(&self, point: Self::Vec2) -> Self::Vec2;
-
-    /// Scaling matrix
     fn from_scale(vector: Self::Vec2) -> Self;
-
-    /// Rotation matrix
     fn from_angle(angle: Self::Scalar) -> Self;
-
-    /// 2D translation matrix.
     fn from_translation(translation: Self::Vec2) -> Self;
-
-    /// 2D transform.
     fn from_scale_angle_translation(
         scale: Self::Vec2,
         angle: Self::Scalar,
@@ -124,54 +74,26 @@ pub trait Matrix3: Matrix + Mul<Self::Vec3, Output = Self::Vec3> {
 /// Implemented for [`glam::Mat4`] and [`glam::DMat4`].
 #[allow(missing_docs)]
 pub trait Matrix4: Matrix + Mul<Self::Vec4, Output = Self::Vec4> {
-    /// Transform point.
-    ///
-    /// See [`glam::Mat4::transform_point3()`] or
-    /// [`glam::DMat4::transform_point3()`].
     fn transform_point3(&self, point: Self::Vec3) -> Self::Vec3;
-
-    /// Transform vector.
-    ///
-    /// See [`glam::Mat4::transform_vector3()`] or
-    /// [`glam::DMat4::transform_vector3()`].
     fn transform_vector3(&self, vector: Self::Vec3) -> Self::Vec3;
-
-    /// Project point.
-    ///
-    /// See [`glam::Mat4::project_point3()`] or
-    /// [`glam::DMat4::project_point3()`].
     fn project_point3(&self, vector: Self::Vec3) -> Self::Vec3;
 
-    /// Create from column vectors.
     fn from_cols(
         x_axis: Self::Vec4,
         y_axis: Self::Vec4,
         z_axis: Self::Vec4,
         w_axis: Self::Vec4,
     ) -> Self;
-
-    /// Get column at `index`.
     fn col(&self, index: usize) -> Self::Vec4;
-
-    /// Get row at `index`.
     fn row(&self, index: usize) -> Self::Vec4;
-
-    /// Scaling matrix
     fn from_scale(vector: Self::Vec3) -> Self;
-
-    /// Rotation matrix
     fn from_axis_angle(axis: Self::Vec3, angle: Self::Scalar) -> Self;
-
-    /// 3D translation matrix.
     fn from_translation(translation: Self::Vec3) -> Self;
-
-    /// Scale, rotation, translation.
     fn from_scale_rotation_translation(
         scale: Self::Vec3,
         axis: <Self::Scalar as PrimitiveMatrices>::Quat,
         translation: Self::Vec3,
     ) -> Self;
-
     fn look_at_lh(eye: Self::Vec3, center: Self::Vec3, up: Self::Vec3) -> Self;
     fn look_at_rh(eye: Self::Vec3, center: Self::Vec3, up: Self::Vec3) -> Self;
     fn perspective_rh_gl(
