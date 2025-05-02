f41aa3cabaffc06be65c547796be960cd3c39f7a
Simon Ask Ulsnes <simon@ulsnes.dk>
2025-05-02 16:55:13 +0200

    Box3 tests

--- src/box.rs
+++ src/box.rs
@@ -680,6 +680,30 @@ impl<T: Unit> Contains<Box2<T>> for Box2<T> {
     }
 }
 
+impl<T: Unit> Contains<Point3<T>> for Box3<T> {
+    #[inline]
+    fn contains(&self, thing: &Point3<T>) -> bool {
+        thing.x >= self.min.x
+            && thing.y >= self.min.y
+            && thing.z >= self.min.z
+            && thing.x <= self.max.x
+            && thing.y <= self.max.y
+            && thing.z <= self.max.z
+    }
+}
+
+impl<T: Unit> Contains<Box3<T>> for Box3<T> {
+    #[inline]
+    fn contains(&self, other: &Box3<T>) -> bool {
+        self.min.x <= other.min.x
+            && self.min.y <= other.min.y
+            && self.min.z <= other.min.z
+            && self.max.x >= other.max.x
+            && self.max.y >= other.max.y
+            && self.max.z >= other.max.z
+    }
+}
+
 impl<T: Unit> Intersection<Point2<T>> for Box2<T> {
     type Intersection = Point2<T>;
 
@@ -1136,7 +1160,7 @@ impl<T: Unit> core::fmt::Debug for Box3<T> {
 mod tests {
     use approx::{assert_abs_diff_eq, assert_ulps_eq};
 
-    use crate::{point, vec2};
+    use crate::{point, vec2, vec3};
 
     use super::*;
 
@@ -1169,6 +1193,11 @@ mod tests {
         let b = Box2::from_array(array);
         assert_eq!(b.min, Point2::ZERO);
         assert_eq!(b.max, Point2::ONE);
+
+        let array = [Point3::ZERO, Point3::ONE];
+        let b = Box3::from_array(array);
+        assert_eq!(b.min, Point3::ZERO);
+        assert_eq!(b.max, Point3::ONE);
     }
 
     #[test]
@@ -1180,10 +1209,19 @@ mod tests {
         let _: &[f32; 4] = b.as_scalar_array();
         let _: &mut [f32; 4] = b.as_scalar_array_mut();
         let _: [f32; 4] = b.to_scalar_array();
+
+        let mut b = Box3::new(Point3::new(0.5, 0.5, 0.5), Point3::new(1.0, 1.0, 1.0));
+        let _: &[Point3; 2] = b.as_array();
+        let _: &mut [Point3; 2] = b.as_array_mut();
+        let _: [Point3; 2] = b.to_array();
+        let _: &[f32; 6] = b.as_scalar_array();
+        let _: &mut [f32; 6] = b.as_scalar_array_mut();
+        let _: [f32; 6] = b.to_scalar_array();
+
     }
 
     #[test]
-    fn from_points() {
+    fn from_points_box2() {
         let empty: [Point2<_>; 0] = [];
         let b = Box2::from_points(empty);
         assert_eq!(b, Box2::ZERO);
@@ -1201,6 +1239,29 @@ mod tests {
     }
 
     #[test]
+    fn from_points_box3() {
+        let empty: [Point3<_>; 0] = [];
+        let b = Box3::from_points(empty);
+        assert_eq!(b, Box3::ZERO);
+
+        let b = Box3::from_points([
+            (0.1, 1.0, 2.0),
+            (1.0, 2.0, 3.0),
+            (0.0, 2.0, 4.0),
+            (0.5, 0.5, 5.0),
+        ]
+        .map(Point3::from));
+
+        assert_abs_diff_eq!(
+            b,
+            Box3 {
+                min: Point3::new(0.0, 0.5, 2.0),
+                max: Point3::new(1.0, 2.0, 5.0),
+            }
+        );
+    }
+
+    #[test]
     fn corners() {
         let b = Box2 {
             min: (-1.0, -1.0).into(),
@@ -1211,10 +1272,24 @@ mod tests {
         assert_eq!(ne, Point2::new(1.0, -1.0));
         assert_eq!(se, Point2::new(1.0, 1.0));
         assert_eq!(sw, Point2::new(-1.0, 1.0));
+
+        let b = Box3 {
+            min: (-1.0, -1.0, -1.0).into(),
+            max: (1.0, 1.0, 1.0).into(),
+        };
+        let [nw, ne, se, sw, nwu, neu, seu, swu] = b.corners();
+        assert_eq!(nw, Point3::new(-1.0, -1.0, -1.0));
+        assert_eq!(ne, Point3::new(1.0, -1.0, -1.0));
+        assert_eq!(se, Point3::new(1.0, 1.0, -1.0));
+        assert_eq!(sw, Point3::new(-1.0, 1.0, -1.0));
+        assert_eq!(nwu, Point3::new(-1.0, -1.0, 1.0));
+        assert_eq!(neu, Point3::new(1.0, -1.0, 1.0));
+        assert_eq!(seu, Point3::new(1.0, 1.0, 1.0));
+        assert_eq!(swu, Point3::new(-1.0, 1.0, 1.0));
     }
 
     #[test]
-    fn negative_empty() {
+    fn negative_empty_box2() {
         let r = IBox2::from_size((-1, -1).into());
         assert!(r.is_empty());
         assert!(r.is_negative());
@@ -1249,7 +1324,42 @@ mod tests {
     }
 
     #[test]
-    fn translate() {
+    fn negative_empty_box3() {
+        let r = IBox3::from_size((-1, -1, -1).into());
+        assert!(r.is_empty());
+        assert!(r.is_negative());
+
+        let r = IBox3::from_size((0, 0, 0).into());
+        assert!(r.is_empty());
+        assert!(!r.is_negative());
+
+        let r = IBox3::from_size((1, 1, 1).into());
+        assert!(!r.is_empty());
+        assert!(!r.is_negative());
+
+        // Negative zero.
+
+        let r = Box3::from_size((-0.0, 0.0, 0.0).into());
+        assert!(r.is_empty());
+        assert!(!r.is_negative());
+
+        let r = Box3::new((1.0, 1.0, 1.0).into(), (-0.0, -0.0, -0.0).into());
+        assert!(r.is_empty());
+        assert!(r.is_negative());
+
+        // NaN
+
+        let r = Box3::from_size((f32::NAN, f32::NAN, f32::NAN).into());
+        assert!(r.is_empty());
+        assert!(r.is_negative());
+
+        let r = Box3::new((f32::NAN, 1.0, 1.0).into(), (1.0, 1.0, 1.0).into());
+        assert!(r.is_empty());
+        assert!(r.is_negative());
+    }
+
+    #[test]
+    fn translate_box2() {
         let b = Box2 {
             min: (1.0, 2.0).into(),
             max: (2.0, 3.0).into(),
@@ -1268,6 +1378,29 @@ mod tests {
         assert_abs_diff_eq!(b3, b2);
         assert_abs_diff_eq!(b3 - vec2![1.0, 0.5], b);
         b3 -= vec2![1.0, 0.5];
+        assert_abs_diff_eq!(b3, b);
+    }
+
+    #[test]
+    fn translate_box3() {
+        let b = Box3 {
+            min: (1.0, 2.0, 3.0).into(),
+            max: (2.0, 3.0, 4.0).into(),
+        };
+        let b2 = b.translate(vec3!(1.0, 0.5, -1.0));
+        assert_abs_diff_eq!(
+            b2,
+            Box3 {
+                min: (2.0, 2.5, 2.0).into(),
+                max: (3.0, 3.5, 3.0).into(),
+            }
+        );
+
+        let mut b3 = b;
+        b3 += vec3![1.0, 0.5, -1.0];
+        assert_abs_diff_eq!(b3, b2);
+        assert_abs_diff_eq!(b3 - vec3![1.0, 0.5, -1.0], b);
+        b3 -= vec3![1.0, 0.5, -1.0];
         assert_abs_diff_eq!(b3, b);
     }
 
@@ -1305,8 +1438,23 @@ mod tests {
 
         assert_abs_diff_eq!(b.size().area(), b.area());
         assert_abs_diff_eq!(b.area(), 4.0);
-
         assert_abs_diff_eq!(Box2::ZERO.area(), 0.0);
+
+        let b = Box3 {
+            min: Point3::ZERO,
+            max: point!(1.0, 2.0, 3.0),
+        };
+        assert_abs_diff_eq!(
+            b.size(),
+            Size3 {
+                width: 1.0,
+                height: 2.0,
+                depth: 3.0,
+            }
+        );
+        assert_abs_diff_eq!(b.size().volume(), b.volume());
+        assert_abs_diff_eq!(b.volume(), 6.0);
+        assert_abs_diff_eq!(Box3::ZERO.volume(), 0.0);
     }
 
     #[test]
@@ -1314,11 +1462,15 @@ mod tests {
         let b = Box2::new((-1.0, -1.0).into(), (1.0, 1.0).into());
         assert!(b.contains(&Point2::new(-1.0, 0.0)));
         assert!(b.contains(&Point2::new(1.0, 1.0)));
+
+        let b = Box3::new(Point3::ZERO, point!(1.0, 2.0, 3.0));
+        assert!(b.contains(&Point3::new(0.0, 1.0, 2.0)));
+        assert!(b.contains(&Point3::new(1.0, 2.0, 3.0)));
     }
 
     #[test]
     #[allow(clippy::many_single_char_names)]
-    fn intersection() {
+    fn intersection_box2() {
         type Box2 = super::Box2<f32>;
         let x = Box2::new((10.0, 10.0).into(), (20.0, 20.0).into());
 
@@ -1433,7 +1585,104 @@ mod tests {
     }
 
     #[test]
-    fn intersection_contained() {
+    fn intersection_box3() {
+        type Box3 = super::Box3<f32>;
+        let x = Box3::new((10.0, 10.0, 10.0).into(), (20.0, 20.0, 20.0).into());
+
+        {
+            // No intersection
+            assert!(x.intersection(&Point3::new(0.0, 0.0, 0.0)).is_none());
+
+            let nw = Box3::new((0.0, 0.0, 0.0).into(), (10.0, 10.0, 10.0).into());
+            let n = Box3::new((10.0, 0.0, 0.0).into(), (20.0, 10.0, 10.0).into());
+            let ne = Box3::new((20.0, 0.0, 0.0).into(), (30.0, 10.0, 10.0).into());
+            let e = Box3::new((20.0, 10.0, 10.0).into(), (30.0, 20.0, 30.0).into());
+            let se = Box3::new((20.0, 20.0, 20.0).into(), (30.0, 30.0, 30.0).into());
+            let s = Box3::new((10.0, 20.0, 20.0).into(), (20.0, 30.0, 30.0).into());
+            let sw = Box3::new((1., -1., -1.).into(), (11., -11., -11.).into());
+            let w = Box3::new((-1., -1., -1.).into(), (11., -11., -11.).into());
+
+            assert_eq!(x.intersection(&nw), None);
+            assert_eq!(x.intersection(&n), None);
+            assert_eq!(x.intersection(&ne), None);
+            assert_eq!(x.intersection(&e), None);
+            assert_eq!(x.intersection(&se), None);
+            assert_eq!(x.intersection(&s), None);
+            assert_eq!(x.intersection(&sw), None);
+            assert_eq!(x.intersection(&w), None);
+
+            assert_eq!(nw.intersection(&x), None);
+            assert_eq!(n.intersection(&x), None);
+            assert_eq!(ne.intersection(&x), None);
+            assert_eq!(e.intersection(&x), None);
+            assert_eq!(se.intersection(&x), None);
+            assert_eq!(s.intersection(&x), None);
+            assert_eq!(sw.intersection(&x), None);
+            assert_eq!(w.intersection(&x), None);
+            assert_eq!(nw.intersection(&x), None);
+            assert_eq!(n.intersection(&x), None);
+            assert_eq!(ne.intersection(&x), None);
+            assert_eq!(e.intersection(&x), None);
+
+            assert_eq!(x.intersection(&se.min), None);
+            assert_eq!(x.intersection(&se.max), None);
+            assert_eq!(se.intersection(&x.min), None);
+            assert_eq!(se.intersection(&x), None);
+            assert_eq!(s.intersection(&x), None);
+            assert_eq!(sw.intersection(&x), None);
+            assert_eq!(w.intersection(&x), None);
+            assert!(!nw.intersects(&x));
+        }
+
+        // Intersections
+        {
+            assert_eq!(
+                x.intersection(&Point3::new(10.0, 10.0, 10.0)),
+                Some(Point3::new(10.0, 10.0, 10.0))
+            );
+
+            // Intersections
+            let nw = Box3::new((0.0, 0.0, 0.0).into(), (11.0, 11.0, 11.0).into());
+            let n = Box3::new((11.0, 0.0, 0.0).into(), (19.0, 11.0, 11.0).into());
+            let ne = Box3::new((19.0, 0.0, 0.0).into(), (29.0, 11.0, 11.0).into());
+            let e = Box3::new((19.0, 11.0, 11.0).into(), (30.0, 29.0, 30.0).into());
+            let se = Box3::new((19.0, 19.0, 19.0).into(), (30.0, 30.0, 30.0).into());
+            let s = Box3::new((11.0, 19.0, 19.0).into(), (19.0, 30.0, 30.0).into());
+            let sw = Box3::new((0.0, 19.0, 19.0).into(), (11.0, 30.0, 30.0).into());
+            let w = Box3::new((0.0, 11.0, 11.0).into(), (11.0, 19.0, 19.0).into());
+
+
+            assert!(nw.intersects(&x));
+            assert!(n.intersects(&x));
+            assert!(ne.intersects(&x));
+            assert!(e.intersects(&x));
+            assert!(se.intersects(&x));
+            assert!(s.intersects(&x));
+            assert!(sw.intersects(&x));
+            assert!(w.intersects(&x));
+
+            assert!(x.intersects(&nw));
+            assert!(x.intersects(&n));
+            assert!(x.intersects(&ne));
+            assert!(x.intersects(&e));
+            assert!(x.intersects(&se));
+            assert!(x.intersects(&s));
+            assert!(x.intersects(&sw));
+            assert!(x.intersects(&w));
+
+            assert_eq!(x.intersection(&nw), nw.intersection(&x));
+            assert_eq!(x.intersection(&n), n.intersection(&x));
+            assert_eq!(x.intersection(&ne), ne.intersection(&x));
+            assert_eq!(x.intersection(&e), e.intersection(&x));
+            assert_eq!(x.intersection(&se), se.intersection(&x));
+            assert_eq!(x.intersection(&s), s.intersection(&x));
+            assert_eq!(x.intersection(&sw), sw.intersection(&x));
+            assert_eq!(x.intersection(&w), w.intersection(&x));
+        }
+    }
+
+    #[test]
+    fn intersection_contained_box2() {
         let large = Box2::new((0.0, 0.0).into(), (100.0, 100.0).into());
         let small = Box2::new((10.0, 10.0).into(), (90.0, 90.0).into());
         assert!(large.intersects(&small));
@@ -1449,7 +1698,23 @@ mod tests {
     }
 
     #[test]
-    fn intersection_partial() {
+    fn intersection_contained_box3() {
+        let large = Box3::new((0.0, 0.0, 0.0).into(), (100.0, 100.0, 100.0).into());
+        let small = Box3::new((10.0, 10.0, 10.0).into(), (90.0, 90.0, 90.0).into());
+        assert!(large.intersects(&small));
+        assert!(small.intersects(&large));
+        assert_eq!(large.intersection(&small), Some(small));
+        assert_eq!(small.intersection(&large), Some(small));
+
+        let infinite = Box3::new(Point3::ZERO, Point3::INFINITY);
+        assert!(infinite.intersects(&small));
+        assert!(small.intersects(&infinite));
+        assert_eq!(infinite.intersection(&small), Some(small));
+        assert_eq!(small.intersection(&infinite), Some(small));
+    }
+
+    #[test]
+    fn intersection_partial_box2() {
         let large = Box2::new((0.0, 0.0).into(), (100.0, 100.0).into());
         let small = Box2::new((90.0, 10.0).into(), (110.0, 20.0).into());
         let expected = Box2::new((90.0, 10.0).into(), (100.0, 20.0).into());
@@ -1460,7 +1725,18 @@ mod tests {
     }
 
     #[test]
-    fn union() {
+    fn intersection_partial_box3() {
+        let large = Box3::new((0.0, 0.0, 0.0).into(), (100.0, 100.0, 100.0).into());
+        let small = Box3::new((90.0, 10.0, 10.0).into(), (110.0, 20.0, 20.0).into());
+        let expected = Box3::new((90.0, 10.0, 10.0).into(), (100.0, 20.0, 20.0).into());
+        assert!(large.intersects(&small));
+        assert!(small.intersects(&large));
+        assert_eq!(large.intersection(&small), Some(expected));
+        assert_eq!(small.intersection(&large), Some(expected));
+    }
+
+    #[test]
+    fn union_box2() {
         let a = Box2 {
             min: (0.0, 0.0).into(),
             max: (1.0, 1.0).into(),
@@ -1480,6 +1756,29 @@ mod tests {
 
         assert_abs_diff_eq!(b.union(Box2::ZERO), b);
         assert_abs_diff_eq!(Box2::ZERO.union(b), b);
+    }
+
+    #[test]
+    fn union_box3() {
+        let a = Box3 {
+            min: (0.0, 0.0, 0.0).into(),
+            max: (1.0, 1.0, 1.0).into(),
+        };
+        let b = Box3 {
+            min: (2.0, 2.0, 2.0).into(),
+            max: (3.0, 3.0, 3.0).into(),
+        };
+
+        assert_abs_diff_eq!(
+            a.union(b),
+            Box3 {
+                min: (0.0, 0.0, 0.0).into(),
+                max: (3.0, 3.0, 3.0).into(),
+            }
+        );
+
+        assert_abs_diff_eq!(b.union(Box3::ZERO), b);
+        assert_abs_diff_eq!(Box3::ZERO.union(b), b);
     }
 
     #[test]
